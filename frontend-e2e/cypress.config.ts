bbb7311857bff1f9a2d762bdeda42d3841475bab
ThauanyAA <thauanyaraujo5@gmail.com>
2026-07-23 20:27:41 -0300

    feat: add AI-driven event registration E2E test and configure Cypress project ID

--- frontend-e2e/cypress.config.ts
+++ frontend-e2e/cypress.config.ts
@@ -1,6 +1,7 @@
 const { nxE2EPreset } = require('@nx/cypress/plugins/cypress-preset');
 const { defineConfig } = require('cypress');
 module.exports = defineConfig({
+  projectId: 'jyktba',
   e2e: {
     ...nxE2EPreset(__filename, {
       cypressDir: 'src',
