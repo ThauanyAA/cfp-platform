d45bd33cb0278976c051084855028f0c76a1f299
ThauanyAA <thauanyaraujo5@gmail.com>
2026-07-23 20:11:02 -0300

    feat: initialize Cypress E2E testing framework and implement event registration test suite

---
+++ frontend-e2e/cypress.config.ts
@@ -0,0 +1,15 @@
+const { nxE2EPreset } = require('@nx/cypress/plugins/cypress-preset');
+const { defineConfig } = require('cypress');
+module.exports = defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run frontend:serve',
+        production: 'nx run frontend:serve:production',
+      },
+      ciWebServerCommand: 'nx run frontend:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
